9b73e3f7317777fad0bc694dffa213b07969b902
Crowdbotics <team@crowdbotics.com>
2023-12-08 15:03:17 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "kz-equine-44912",
+  url: "https://kz-equine-44912.botics.co",
+  api: "https://kz-equine-44912.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
